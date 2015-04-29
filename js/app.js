fdfcba3523708c15931aea52be183ac15085f611
Connor Franklin <connorbfranklin@gmail.com>
2015-04-29 16:06:54 -0400

    cleanup css, QA hashchange

--- js/app.js
+++ js/app.js
@@ -151,6 +151,7 @@ $(function() {
         if(typeof ga !== "undefined"){
             ga('send', 'pageview', '/fa/'+window.location.hash);
         }
+        console.log('hashchange!')
     });
 });
 
